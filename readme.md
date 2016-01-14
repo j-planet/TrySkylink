3f4ce1b6f7e173e4a7f1831f475bf033bbdc46ba
jennyyuejin <jin.yuejenny@gmail.com>
2016-01-13 23:01:11 -0800

    added server. updated readme.

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 #A very simple video chat web app that uses SkylinkJS and React Redux
+
+1. Install packages using
+`npm install`
+
+2. Run in development mode:
+    ```NODE_ENV=development node server.js```
+
+3. Run in production mode:
+    ```NODE_ENV=production node server.js```
+    
+4. Open the application on [http://localhost:3000](http://localhost:3000).
---
+++ server.js
@@ -0,0 +1,60 @@
+/**
+ * Created by jj on 1/13/16.
+ */
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var static_path = path.join(__dirname, 'public');
+var defaultPort = 3000;
+
+
+if (process.env.NODE_ENV == 'production')       // prod environment
+{
+    console.log('RUNNING PROD MODE');
+    app.use(express.static(static_path));
+}
+else if (process.env.NODE_ENV == 'development')        // dev environment
+{
+    console.log('RUNNING DEV MODE');
+
+    // ----- do NOT use in production!!!! ------
+
+    var webpack = require('webpack');
+    var config = require('./webpack-dev.config.js');
+    var compiler = webpack(config);
+
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo: false,
+        quiet: false,
+        publicPath: config.output.path,
+        stats: {
+            colors: true
+        }
+    }));
+
+    app.use(require('webpack-hot-middleware')(compiler));
+
+}
+else
+{
+    console.error('UNRECOGNIZED NODE_ENV: ' + process.env.NODE_ENV);
+}
+
+
+app
+    .get('*', function (req, res)
+    {
+        console.log('get called');
+
+        res.sendFile('index.html', {
+            root: static_path
+        });
+    })
+    .listen(process.env.PORT || defaultPort, function (err)
+    {
+        if (err) { console.log(err); }
+        console.log('Listening at localhost:' + (process.env.PORT || defaultPort));
+    });
+
